80a4a0255c11a0b848987f116296d8c298dffc59
Akshay Dhiman <hello@akshaydhiman.com>
2022-12-21 22:20:47 +0530

    home animation update

--- scripts/main.js
+++ scripts/main.js
@@ -2,35 +2,67 @@
 
 window.gsap.registerPlugin(window.ScrollTrigger);
 
-const appearOnScroll = window.gsap.utils.toArray(".appear-on-scroll");
-const zoomImageOnScroll = window.gsap.utils.toArray(".zoom-image-on-scroll");
+const appearOnScroll = [
+  window.gsap.utils.toArray(".appear-on-scroll-1"),
+  window.gsap.utils.toArray(".appear-on-scroll-2"),
+  window.gsap.utils.toArray(".appear-on-scroll-3"),
+  window.gsap.utils.toArray(".appear-on-scroll-4"),
+  window.gsap.utils.toArray(".appear-on-scroll-5"),
+  window.gsap.utils.toArray(".appear-on-scroll-6"),
+];
+const zoomImageOnScroll = [
+  window.gsap.utils.toArray(".zoom-image-on-scroll-1"),
+  window.gsap.utils.toArray(".zoom-image-on-scroll-2"),
+  window.gsap.utils.toArray(".zoom-image-on-scroll-3"),
+];
 
-appearOnScroll.forEach((trigger, index) => {
-  window.gsap.from(trigger, {
-    delay: 0.1 * index,
-    duration: 2,
-    opacity: 0,
-    y: 150,
-    ease: "power2.out",
-    scrollTrigger: {
-      trigger,
-      start: "top bottom",
-    },
+appearOnScroll.forEach((group) => {
+  group.forEach((trigger, index) => {
+    window.gsap.from(trigger, {
+      delay: 0.1 * index,
+      duration: 2,
+      opacity: 0,
+      y: 150,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger,
+        start: "top bottom",
+      },
+    });
   });
 });
 
-zoomImageOnScroll.forEach((image, index) => {
-  window.gsap.from(image, {
-    delay: 0.1 * index,
-    duration: 2,
-    scale: 1.3,
-    ease: "power2.out",
-    scrollTrigger: {
-      trigger: image,
-      start: "top bottom",
-    },
+zoomImageOnScroll.forEach((group) => {
+  group.forEach((image, index) => {
+    window.gsap.from(image, {
+      delay: 0.1 * index,
+      duration: 2,
+      scale: 1.3,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: image,
+        start: "top bottom",
+      },
+    });
   });
 });
+
+const homeActionScrollTrigger = {
+  trigger: ".home-action",
+  start: "top bottom",
+  end: "bottom top",
+  scrub: true,
+};
+
+window.gsap.to(".home-action img", {
+  y: -200,
+  scrollTrigger: homeActionScrollTrigger,
+});
+
+// window.gsap.fromTo(
+//   ".home-action > div",
+//   { y: -50, scrollTrigger: homeActionScrollTrigger }
+// );
 
 /* Sliders */
 
