f0f1418f3c784062abd17acafec5c18577cd8ce6
Akshay Dhiman <hello@akshaydhiman.com>
2023-01-31 19:27:05 +0530

    podcast content update

--- scripts/main.js
+++ scripts/main.js
@@ -774,3 +774,21 @@ if (window.location.pathname.startsWith("/team")) {
     }
   });
 }
+
+/* Podcast */
+
+document.querySelector("#trigger-play").addEventListener("click", () => {
+  new YT.Player("player", {
+    height: "390",
+    width: "640",
+    videoId: "M7lc1UVf-VE",
+    playerVars: {
+      playsinline: 1,
+    },
+    events: {
+      onReady: (event) => {
+        event.target.playVideo();
+      },
+    },
+  });
+});
